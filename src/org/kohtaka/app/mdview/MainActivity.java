b69993f81aacd94d479cc0c5d3dc35db6e02f15c
Kazumasa Kohtaka <kkohtaka@gmail.com>
2013-09-28 21:01:59 +0900

    add bootstrap.css

--- src/org/kohtaka/app/mdview/MainActivity.java
+++ src/org/kohtaka/app/mdview/MainActivity.java
@@ -5,6 +5,7 @@ package org.kohtaka.app.mdview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,9 +15,11 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    String markdown = "# TITLE\n## SECTION\n* ITEM1\n* ITEM2\n* ITEM3";
-    String html = DiscountBinding.convertMarkdownToHtml(markdown);
-    if (html.length() > 0) {
+    String mdString = "# TITLE\n## SECTION\n* ITEM1\n* ITEM2\n* ITEM3";
+    String htmlString = generateHtmlString(
+        DiscountBinding.convertMarkdownToHtml(mdString));
+
+    if (htmlString.length() > 0) {
       WebView webView = (WebView)findViewById(R.id.webview);
       // Disable clicking links
       webView.setWebViewClient(new WebViewClient() {
@@ -24,8 +27,20 @@ public class MainActivity extends Activity {
           return true;
         }
       });
-      webView.loadData(html, "text/html; charset=UTF-8", null);
+      webView.loadDataWithBaseURL(
+          "file:///android_asset/",
+          htmlString,
+          "text/html",
+          "UTF-8",
+          null);
     }
+  }
+
+  private String generateHtmlString(String bodyString) {
+    return "<!DOCTYPE html>" +
+        "<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />" +
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.min.css\">" +
+        bodyString;
   }
 }
 
